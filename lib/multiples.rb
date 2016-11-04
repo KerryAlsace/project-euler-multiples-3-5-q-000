667d0f437123d114a3e73d9ae649fd11d27dfcc8
KerryAlsace <kerrydougherty@gmail.com>
2016-11-04 13:46:12 -0400

    Done.

--- lib/multiples.rb
+++ lib/multiples.rb
@@ -1,1 +1,15 @@
 # Enter your procedural solution here!
+def sum_multiples
+	sum = 0
+	(1..999).each do |num|
+		if num % 15 == 0
+			sum += num
+		elsif (num % 3 == 0) || (num % 5 == 0)
+			sum += num
+		end
+	end
+	sum
+end
+
+puts sum_multiples
+# should return 233168
